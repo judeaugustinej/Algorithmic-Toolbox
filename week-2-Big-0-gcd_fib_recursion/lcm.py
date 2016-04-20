8c1d7995cc3b9bc5355864d61d2cb2695439747a
Jude Augustine Job <judeopenstack@gmail.com>
2016-04-20 17:32:50 +0530

    Create lcm.py

---
+++ week-2-Big-0-gcd_fib_recursion/lcm.py
@@ -0,0 +1,11 @@
+# Uses python3
+from fractions import gcd
+import sys
+
+a,b = map(int,sys.stdin.readline().split())
+#n = int(input())
+#m = int(input())
+#print(n)
+#print(m)
+lcm = a*b // gcd(a, b)
+print(lcm)
